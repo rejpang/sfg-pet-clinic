5e87521e9561f450efc5e4556231aef157379f27
rejpang <rejpang@gmail.com>
2022-09-05 22:50:51 +0800

    Create Pet Clinic Controller Add Pet Clinic Index Page and Controller Closes #7 Closes #8

--- sfg-pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
+++ sfg-pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
@@ -1,2 +1,14 @@
-package guru.springframework.sfgpetclinic.controllers;public class IndexController {
+package guru.springframework.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String index(){
+        return "index";
+    }
+
 }
